63b9bfdd78c79e64d64e3d0d3985900091fbb79b
EricoBis <ericobis.santos@gmail.com>
2022-11-17 12:32:27 -0300

    change text align of Ratings

--- src/components/ratings/style.js
+++ src/components/ratings/style.js
@@ -7,10 +7,14 @@ export const RatingContainer = styled.div`
     background-color: #f3f3f3;
     border-radius: 10px;
     padding: 12px;
-    margin: 12px;
+    margin: 12px 1%;
     p{
         font-size: 12px;
         margin: 0;
+        overflow-wrap: break-word;  
+        word-wrap: break-word; 
+        word-break: break-word;
+        text-align: center;
     }
     svg{
         font-size: 30px;
