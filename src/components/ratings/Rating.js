1a66cfa0845ac3ee3366602eb578a70d274b3f05
Érico Bis <ericobis.santos@gmail.com>
2022-11-17 12:27:25 +0000

    change Rating.js

--- src/components/ratings/Rating.js
+++ src/components/ratings/Rating.js
@@ -6,29 +6,41 @@ import { SiRottentomatoes } from "react-icons/si";
 import { FaImdb } from "react-icons/fa";
 import { RatingContainer } from "./style";
 
+const RATER = {
+  imdb: "Internet Movie Database",
+  rotten: "Rotten Tomatoes",
+};
+
 function Rating({ value }) {
+  const formatSource = (source) => {
+    if (source === RATER.imdb) return "IMDb";
+    else if (source === RATER.rotten) return "Rotten T.";
+    else return source;
+  };
+
+  const getIcon = (source) => {
+    if (source === RATER.imdb) return <FaImdb />;
+    else if (source === RATER.rotten) return <SiRottentomatoes />;
+  };
+
   return (
     <Flex dir="row">
-      {value[0] && (
-        <RatingContainer>
-          <Flex dir="row" align="center">
-            <FaImdb />
-            <h3>{value[0].Value}</h3>
-          </Flex>
-          <p>IMDb</p>
-        </RatingContainer>
-      )}
-      {value[1] && (
-        <RatingContainer>
-          <Flex dir="row" align="center">
-            <SiRottentomatoes />
-            <h3>{value[1].Value}</h3>
-          </Flex>
-          <p>Rotten Tomatoes</p>
-        </RatingContainer>
-      )}
+      {value &&
+        value.map((rating) => {
+          const { Source, Value } = rating;
+          return (
+            <RatingContainer>
+              <Flex dir="row" align="center">
+                {getIcon(Source)}
+                <h3>{Value}</h3>
+              </Flex>
+              <p>{formatSource(Source)}</p>
+            </RatingContainer>
+          );
+        })}
     </Flex>
   );
 }
 
 export default Rating;
+
